2dbb01b7c524f4fd26eb72ad274dd3b6906fba06
hackzfy <hackzfy@gmail.com>
2022-12-05 14:31:23 +0800

    calclate eth price

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,21 +4,31 @@ pragma solidity ^0.8.12;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18;
 
     function fund() public payable{
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
-        require(msg.value >= 1e9, "Didn't send enough. Current amount send");
+        require(getConvertion(msg.value) >= minimumUsd, "Didn't send enough. Current amount send");
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // ABI
         // Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 3000_00000000
+        return uint256(price * 1e10);
     }
 
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
+
+    function getConvertion(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
     }
+
 }
